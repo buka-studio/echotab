8b2e0f3af86eb1745dd06ff44d86a69972e15cdd
rpavlini <robert.pavlinic95@gmail.com>
2024-08-21 21:40:17 +0200

    fly & ci wip

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,37 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    // transpilePackages: ["@echotab/ui"],
-    experimental: {
-        externalDir: true,
-    },
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/i,
-            use: [
+  reactStrictMode: true,
+  transpilePackages: ["@echotab/ui"],
+  experimental: {
+    externalDir: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
                 {
-                    loader: "@svgr/webpack",
-                    options: {
-                        svgoConfig: {
-                            plugins: [
-                                {
-                                    name: "preset-default",
-                                    params: {
-                                        overrides: {
-                                            cleanupIds: false,
-                                            removeViewBox: false,
-                                        },
-                                    },
-                                },
-                            ],
-                        },
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      cleanupIds: false,
+                      removeViewBox: false,
                     },
+                  },
                 },
-            ],
-        });
+              ],
+            },
+          },
+        },
+      ],
+    });
 
-        return config;
-    },
+    return config;
+  },
 };
 
 export default nextConfig;
