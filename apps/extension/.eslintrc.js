fcdfdf7e11e1fe1f98ce1b58a5a1ca56c8be4f13
rpavlini <robert.pavlinic95@gmail.com>
2024-08-21 21:37:34 +0200

    eslint pkg

--- apps/extension/.eslintrc.js
+++ apps/extension/.eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   root: true,
-  extends: ["next/core-web-vitals", "plugin:valtio/recommended"],
+  extends: ["next/core-web-vitals", "plugin:valtio/recommended", "@echotab/eslint"],
 };
