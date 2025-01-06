2149a90848abacb875a595f088259c8380966826
rpavlini <robert.pavlinic95@gmail.com>
2025-01-06 21:44:31 +0100

    rename pinned component

--- apps/extension/src/Bookmarks/Pinned.tsx
+++ apps/extension/src/Bookmarks/Pinned.tsx
@@ -8,7 +8,7 @@ import { useBookmarkStore } from "./BookmarkStore";
 import ItemGrid from "./ItemGrid";
 import SavedTabItem from "./SavedTabItem";
 
-export default function SavedTabs() {
+export default function Pinned() {
   const bookmarkStore = useBookmarkStore();
 
   const [expanded, setExpanded] = useState(true);
@@ -18,17 +18,17 @@ export default function SavedTabs() {
       <div className="mb-2 flex select-none items-center px-2 text-sm">
         <span className="mr-2 inline-flex gap-2">
           <span className="text-muted-foreground flex items-center gap-2">
-            <DrawingPinFilledIcon /> Pinned Tabs
+            <DrawingPinFilledIcon /> Pinned
           </span>
           <Badge variant="card">{bookmarkStore.pinnedTabs?.length}</Badge>
         </span>
         <Button
           variant="ghost"
+          size="icon-sm"
           onClick={() => {
             setExpanded(!expanded);
           }}>
-          {expanded ? "Collapse" : "Expand"}
-          <CaretSortIcon className="ml-2 h-4 w-4" />
+          <CaretSortIcon className="h-4 w-4" />
         </Button>
       </div>
 
@@ -40,8 +40,8 @@ export default function SavedTabs() {
               count={5}
               className="[&_.items-placeholder]:max-h-[110px]">
               <ItemListPlaceholderCopy
-                title="No pinned tabs yet."
-                subtitle="Pin a tab by clicking the pin icon on a saved tab."
+                title="No pinned links yet."
+                subtitle="Pin a link by clicking the pin icon on a saved link."
               />
             </ItemListPlaceholder>
           )}
