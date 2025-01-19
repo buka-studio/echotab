6a5224729dcc36884882293ab535dc8124f844cd
rpavlini <robert.pavlinic95@gmail.com>
2025-01-19 01:21:47 +0100

    nav menu reorder

--- apps/extension/src/NavMenu.tsx
+++ apps/extension/src/NavMenu.tsx
@@ -15,27 +15,12 @@ import "@echotab/ui/globals.css";
 import "./app.css";
 
 import ButtonWithTooltip from "@echotab/ui/ButtonWithTooltip";
+import { ReactNode } from "react";
 
-export default function NavMenu() {
+export default function NavMenu({ children }: { children: ReactNode }) {
   return (
     <div className="flex gap-2">
-      <Dialog>
-        <DialogTrigger asChild>
-          <ButtonWithTooltip
-            tooltipText="Settings"
-            variant="outline"
-            size="icon"
-            className="rounded-full">
-            <GearIcon className="h-4 w-4" />
-          </ButtonWithTooltip>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogTitle className="sr-only">Settings</DialogTitle>
-          <DialogDescription className="sr-only">Change your extension settings.</DialogDescription>
-          <Settings />
-        </DialogContent>
-      </Dialog>
-
+      {children}
       <Dialog>
         <DialogTrigger asChild>
           <ButtonWithTooltip
@@ -52,6 +37,22 @@ export default function NavMenu() {
             <DialogDescription>View statistics for your bookmarks and tags.</DialogDescription>
           </DialogHeader>
           <Stats />
+        </DialogContent>
+      </Dialog>
+      <Dialog>
+        <DialogTrigger asChild>
+          <ButtonWithTooltip
+            tooltipText="Settings"
+            variant="outline"
+            size="icon"
+            className="rounded-full">
+            <GearIcon className="h-4 w-4" />
+          </ButtonWithTooltip>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogTitle className="sr-only">Settings</DialogTitle>
+          <DialogDescription className="sr-only">Change your extension settings.</DialogDescription>
+          <Settings />
         </DialogContent>
       </Dialog>
     </div>
