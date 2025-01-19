a6e4d6337742cc6cccadc437b2b5aa738f828aae
rpavlini <robert.pavlinic95@gmail.com>
2025-01-19 01:27:06 +0100

    curate stats WIP

--- apps/extension/src/Stats/Stats.tsx
+++ apps/extension/src/Stats/Stats.tsx
@@ -3,12 +3,14 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@echotab/ui/Tabs";
 
 import TagChip from "../components/tag/TagChip";
 import { unassignedTag, useTagStore } from "../TagStore";
+import CurateStats from "./CurateStats";
 import TagRelationshipGraph from "./TagRelationshipGraph";
 import TagUsageGraph from "./TagUsageGraph";
 
 enum StatTab {
   Relationships = "Relationships",
   Usage = "Usage",
+  Curate = "Curate",
 }
 
 export default function Stats() {
@@ -20,14 +22,18 @@ export default function Stats() {
     <div ref={observerRef} className="flex flex-col gap-5 overflow-hidden">
       <Tabs defaultValue={StatTab.Usage} className="flex-1">
         <TabsList>
-          <TabsTrigger value={StatTab.Usage}>Tag Usage</TabsTrigger>
+          <TabsTrigger value={StatTab.Usage}>Tag Count</TabsTrigger>
           <TabsTrigger value={StatTab.Relationships}>Tag Relationships</TabsTrigger>
+          <TabsTrigger value={StatTab.Curate}>Curated</TabsTrigger>
         </TabsList>
         <TabsContent value={StatTab.Usage} className="focus-visible:ring-0">
           <TagUsageGraph height={500} width={dimensions.width!} />
         </TabsContent>
         <TabsContent value={StatTab.Relationships} className="focus-visible:ring-0">
           <TagRelationshipGraph centerSize={20} height={500} width={dimensions.width!} />
+        </TabsContent>
+        <TabsContent value={StatTab.Curate} className="focus-visible:ring-0">
+          <CurateStats className="h-[500px] w-full" />
         </TabsContent>
       </Tabs>
       {showTags && (
